93d745e8a58f364d12c28c4b425a2bbe7fd35272
AyousefN <ayousef@noventapp.com>
2017-11-21 10:51:50 +0200

    3th Commit

--- routes/api.php
+++ routes/api.php
@@ -21,111 +21,111 @@
 		return $request->user ();
 	} );
 	Route::group ( ['namespace' => 'api'] , function () {
-		Route::get ( 'address' , 'AddressController@index' );
-		Route::post ( 'address' , 'AddressController@add_addresss' );
-		Route::put ( 'address/{id}' , 'AddressController@update_address' );
-		Route::post ( 'users/login' , 'UserController@login' );
-		Route::post ( 'users/logout' , 'UserController@logout' );
+		Route::get ( 'v1/address' , 'AddressController@index' );
+		Route::post ( 'v1/address' , 'AddressController@add_addresss' );
+		Route::put ( 'v1/address/{id}' , 'AddressController@update_address' );
+		Route::post ( 'v1/users/login' , 'UserController@login' );
+		Route::post ( 'v1/users/logout' , 'UserController@logout' );
 		//		Route::post ('users/details', 'UserController@details');
-		Route::resource ( 'users' , 'UserController' );
+		Route::resource ( 'v1/users' , 'UserController' );
 
 		if ( Input::has ( 'phone' ) and !Input::has ( 'email' ) )
-			Route::get ( 'users' , 'UserController@get_phone' );
+			Route::get ( 'v1/users' , 'UserController@get_phone' );
 		else if ( Input::has ( 'start_date' ) or Input::has ( 'end_date' ) and Input::has ( 'status' ) )
-			Route::get ( 'users' , 'UserController@get_date' );
+			Route::get ( 'v1/users' , 'UserController@get_date' );
 		else if ( Input::has ( 'date' ) )
-			Route::get ( 'users' , 'UserController@get_user_by_date' );
+			Route::get ( 'v1/users' , 'UserController@get_user_by_date' );
 		else if ( Input::has ( 'email' ) and !Input::has ( 'phone' ) )
-			Route::get ( 'users' , 'UserController@get_user_email' );
+			Route::get ( 'v1/users' , 'UserController@get_user_email' );
 		else if ( Input::has ( 'phone' ) and Input::has ( 'email' ) )
-			Route::get ( 'users' , 'UserController@get_email_phone' );
+			Route::get ( 'v1/users' , 'UserController@get_email_phone' );
 		else if ( Input::has ( 'status' ) )
-			Route::get ( 'users' , 'UserController@get_inactives_users' );
+			Route::get ( 'v1/users' , 'UserController@get_inactives_users' );
 		else
-			Route::resource ( 'users' , 'UserController' );
+			Route::resource ( 'v1/users' , 'UserController' );
 
 
-		Route::post ( 'suppliers/login' , 'SupplierController@login' );
-		Route::post ( 'suppliers/logout' , 'SupplierController@logout' );
+		Route::post ( 'v1/suppliers/login' , 'SupplierController@login' );
+		Route::post ( 'v1/suppliers/logout' , 'SupplierController@logout' );
 		//		Route::post ('users/details', 'UserController@details');
-		Route::resource ( 'suppliers' , 'SupplierController' );
+		Route::resource ( 'v1/suppliers' , 'SupplierController' );
 
 		if ( Input::has ( 'phone' ) and !Input::has ( 'email' ) )
-			Route::get ( 'suppliers' , 'SupplierController@get_phone' );
+			Route::get ( 'v1/suppliers' , 'SupplierController@get_phone' );
 		else if ( Input::has ( 'start_date' ) or Input::has ( 'end_date' ) and Input::has ( 'status' ) )
-			Route::get ( 'suppliers' , 'SupplierController@get_date' );
+			Route::get ( 'v1/suppliers' , 'SupplierController@get_date' );
 		else if ( Input::has ( 'date' ) )
-			Route::get ( 'suppliers' , 'SupplierController@get_user_by_date' );
+			Route::get ( 'v1/suppliers' , 'SupplierController@get_user_by_date' );
 		else if ( Input::has ( 'email' ) and !Input::has ( 'phone' ) )
-			Route::get ( 'suppliers' , 'SupplierController@get_user_email' );
+			Route::get ( 'v1/suppliers' , 'SupplierController@get_user_email' );
 		else if ( Input::has ( 'phone' ) and Input::has ( 'email' ) )
-			Route::get ( 'suppliers' , 'SupplierController@get_email_phone' );
+			Route::get ( 'v1/suppliers' , 'SupplierController@get_email_phone' );
 		else if ( Input::has ( 'status' ) )
-			Route::get ( 'suppliers' , 'SupplierController@get_inactives_users' );
+			Route::get ( 'v1/suppliers' , 'SupplierController@get_inactives_users' );
 
 		else if ( Input::has ( 'service_id' ) )
-			Route::get ( 'suppliers' , 'SupplierController@suppliers_services_id_s' );
+			Route::get ( 'v1/suppliers' , 'SupplierController@suppliers_services_id_s' );
 		else
-			Route::resource ( 'suppliers' , 'SupplierController' );
+			Route::resource ( 'v1/suppliers' , 'SupplierController' );
 
 
-		Route::post ( 'admins/login' , 'AdminController@login' );
-		Route::post ( 'admins/logout' , 'AdminController@logout' );
+		Route::post ( 'v1/admins/login' , 'AdminController@login' );
+		Route::post ( 'v1/admins/logout' , 'AdminController@logout' );
 		//		Route::post ('users/details', 'UserController@details');
-		Route::resource ( 'admins' , 'AdminController' );
+		Route::resource ( 'v1/admins' , 'AdminController' );
 
 		if ( Input::has ( 'phone' ) and !Input::has ( 'email' ) )
-			Route::get ( 'admins' , 'AdminController@get_phone' );
+			Route::get ( 'v1/admins' , 'AdminController@get_phone' );
 		else if ( Input::has ( 'start_date' ) or Input::has ( 'end_date' ) and Input::has ( 'status' ) )
-			Route::get ( 'admins' , 'AdminController@get_date' );
+			Route::get ( 'v1/admins' , 'AdminController@get_date' );
 		else if ( Input::has ( 'date' ) )
-			Route::get ( 'admins' , 'AdminController@get_user_by_date' );
+			Route::get ( 'v1/admins' , 'AdminController@get_user_by_date' );
 		else if ( Input::has ( 'email' ) and !Input::has ( 'phone' ) )
-			Route::get ( 'admins' , 'AdminController@get_user_email' );
+			Route::get ( 'v1/admins' , 'AdminController@get_user_email' );
 		else if ( Input::has ( 'phone' ) and Input::has ( 'email' ) )
-			Route::get ( 'admins' , 'AdminController@get_email_phone' );
+			Route::get ( 'v1/admins' , 'AdminController@get_email_phone' );
 		else if ( Input::has ( 'status' ) )
-			Route::get ( 'admins' , 'AdminController@get_inactives_users' );
+			Route::get ( 'v1/admins' , 'AdminController@get_inactives_users' );
 		else
-			Route::resource ( 'admins' , 'AdminController' );
+			Route::resource ( 'v1/admins' , 'AdminController' );
 
 
 		//		Route::post ('users/details', 'UserController@details');
-		Route::resource ( 'services' , 'ServicesController' );
+		Route::resource ( 'v1/services' , 'ServicesController' );
 
 		if ( Input::has ( 'section_id' ) and !Input::has ( 'supplier_id' ) )
-			Route::get ( 'services' , 'ServicesController@show_by_section_id' );
+			Route::get ( 'v1/services' , 'ServicesController@show_by_section_id' );
 		else if ( Input::has ( 'suppliers' ) and !Input::has ( 'service_id' ) )
-			Route::get ( 'services' , 'ServicesController@services_suppiler' );
+			Route::get ( 'v1/services' , 'ServicesController@services_suppiler' );
 		else if ( Input::has ( 'supplier_id' ) and !Input::has ( 'section_id' ) )
-			Route::get ( 'services' , 'ServicesController@services_supplier_id' );
+			Route::get ( 'v1/services' , 'ServicesController@services_supplier_id' );
 
 		else if ( Input::has ( 'supplier_id' ) and Input::has ( 'section_id' ) )
-			Route::get ( 'services' , 'ServicesController@services_section_supplier_id_s' );
+			Route::get ( 'v1/services' , 'ServicesController@services_section_supplier_id_s' );
 
 //		  else if(Input::has('supplier_id') and Input::has('service_id'))
 		else
-			Route::resource ( 'services' , 'ServicesController' );
+			Route::resource ( 'v1/services' , 'ServicesController' );
 
 
-		Route::post ( 'services/assign' , 'ServicesController@assigned_services' );
+		Route::post ( 'v1/services/assign' , 'ServicesController@assigned_services' );
 
-		Route::post ( 'services/unassigned' , 'ServicesController@unAssigned_services');
+		Route::post ( 'v1/services/unassigned' , 'ServicesController@unAssigned_services');
 
 
 		//		Route::post ('users/details', 'UserController@details');
-		Route::resource ( 'section' , 'SectionController' );
+		Route::resource ( 'v1/section' , 'SectionController' );
 
 		if ( Input::has ( 'section_id' ) )
-			Route::get ( 'section' , 'SectionController@get_section_id' );
+			Route::get ( 'v1/section' , 'SectionController@get_section_id' );
 
 		else if ( Input::has ( 'service' ) )
-			Route::get ( 'section' , 'SectionController@section_with_service' );
+			Route::get ( 'v1/section' , 'SectionController@section_with_service' );
 
 
 
 
-		Route::resource ( 'orders' , 'OrderController' );
+		Route::resource ( 'v1/orders' , 'OrderController' );
 
 		/*else if (Input::has ('date'))
 Route::get ('section', 'SectionController@get_date_one_Query');
